ae984c69beb11c73084ef8aa5f5a353b4d9515d6
vasilfartsov <vas4o31@abv.bg>
2020-12-07 13:11:06 +0000

    Uebung10

--- src/exercise10/Rectangle.java
+++ src/exercise10/Rectangle.java
@@ -39,7 +39,16 @@ public class Rectangle extends Figure {
 
     @Override
     public boolean containsClick(Point click) {
-        // Check if click point is inside the rectangle
+         boolean xInside = (click.getX()>=startPoint.getX()&&
+        click.getX()<=startPoint.getX()+side2);
+        boolean yInside = (click.getY()>=startPoint.getY()&&
+        click.getY()<=startPoint.getY()+side2);
+
+        if(xInside && yInside){
+          return true;
+        }
+        else{
         return false;
+        }
     }
 }
